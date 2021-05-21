b0e7e7e59e421c2329784ed9d34df189becd55d8
yj76461 <yj76461@gmail.com>
2021-05-21 19:11:50 +0900

    컴퓨터비전12주차세선화과제

--- src.cpp
+++ src.cpp
@@ -3,6 +3,126 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <math.h>
+
+//2차원 배열 동적할당 위함
+unsigned char** imageMatrix;
+// 이진영상에서 
+unsigned char blankPixel = 255, imagePixel = 0;
+
+typedef struct {
+	int row, col;
+}pixel;
+
+int getBlackNeighbours(int row, int col) {
+
+	int i, j, sum = 0;
+
+	for (i = -1; i <= 1; i++) {
+		for (j = -1; j <= 1; j++) {
+			if (i != 0 || j != 0)
+				sum += (imageMatrix[row + i][col + j] == imagePixel);
+		}
+	}
+
+	return sum;
+}
+
+int getBWTransitions(int row, int col) {
+	return 	((imageMatrix[row - 1][col] == blankPixel && imageMatrix[row - 1][col + 1] == imagePixel)
+		+ (imageMatrix[row - 1][col + 1] == blankPixel && imageMatrix[row][col + 1] == imagePixel)
+		+ (imageMatrix[row][col + 1] == blankPixel && imageMatrix[row + 1][col + 1] == imagePixel)
+		+ (imageMatrix[row + 1][col + 1] == blankPixel && imageMatrix[row + 1][col] == imagePixel)
+		+ (imageMatrix[row + 1][col] == blankPixel && imageMatrix[row + 1][col - 1] == imagePixel)
+		+ (imageMatrix[row + 1][col - 1] == blankPixel && imageMatrix[row][col - 1] == imagePixel)
+		+ (imageMatrix[row][col - 1] == blankPixel && imageMatrix[row - 1][col - 1] == imagePixel)
+		+ (imageMatrix[row - 1][col - 1] == blankPixel && imageMatrix[row - 1][col] == imagePixel));
+}
+
+int zhangSuenTest1(int row, int col) {
+	int neighbours = getBlackNeighbours(row, col);
+
+	return ((neighbours >= 2 && neighbours <= 6)
+		&& (getBWTransitions(row, col) == 1)
+		&& (imageMatrix[row - 1][col] == blankPixel || imageMatrix[row][col + 1] == blankPixel || imageMatrix[row + 1][col] == blankPixel)
+		&& (imageMatrix[row][col + 1] == blankPixel || imageMatrix[row + 1][col] == blankPixel || imageMatrix[row][col - 1] == blankPixel));
+}
+
+int zhangSuenTest2(int row, int col) {
+	int neighbours = getBlackNeighbours(row, col);
+
+	return ((neighbours >= 2 && neighbours <= 6)
+		&& (getBWTransitions(row, col) == 1)
+		&& (imageMatrix[row - 1][col] == blankPixel || imageMatrix[row][col + 1] == blankPixel || imageMatrix[row][col - 1] == blankPixel)
+		&& (imageMatrix[row - 1][col] == blankPixel || imageMatrix[row + 1][col] == blankPixel || imageMatrix[row][col + 1] == blankPixel));
+}
+
+void zhangSuen(unsigned char* image, unsigned char* output, int rows, int cols) {
+
+	int startRow = 1, startCol = 1, endRow, endCol, i, j, count, processed;
+
+	pixel* markers;
+
+	imageMatrix = (unsigned char**)malloc(rows * sizeof(unsigned char*));
+
+	for (i = 0; i < rows; i++) {
+		imageMatrix[i] = (unsigned char*)malloc((cols + 1) * sizeof(unsigned char));
+		for (int k = 0; k < cols; k++) imageMatrix[i][k] = image[i * cols + k];
+	}
+
+	endRow = rows - 2;
+	endCol = cols - 2;
+	do {
+		markers = (pixel*)malloc((endRow - startRow + 1) * (endCol - startCol + 1) * sizeof(pixel));
+		count = 0;
+
+		for (i = startRow; i <= endRow; i++) {
+			for (j = startCol; j <= endCol; j++) {
+				if (imageMatrix[i][j] == imagePixel && zhangSuenTest1(i, j) == 1) {
+					markers[count].row = i;
+					markers[count].col = j;
+					count++;
+				}
+			}
+		}
+
+		processed = (count > 0);
+
+		for (i = 0; i < count; i++) {
+			imageMatrix[markers[i].row][markers[i].col] = blankPixel;
+		}
+
+		free(markers);
+		markers = (pixel*)malloc((endRow - startRow + 1) * (endCol - startCol + 1) * sizeof(pixel));
+		count = 0;
+
+		for (i = startRow; i <= endRow; i++) {
+			for (j = startCol; j <= endCol; j++) {
+				if (imageMatrix[i][j] == imagePixel && zhangSuenTest2(i, j) == 1) {
+					markers[count].row = i;
+					markers[count].col = j;
+					count++;
+				}
+			}
+		}
+
+		if (processed == 0)
+			processed = (count > 0);
+
+		for (i = 0; i < count; i++) {
+			imageMatrix[markers[i].row][markers[i].col] = blankPixel;
+		}
+
+		free(markers);
+	} while (processed == 1);
+
+
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < cols; j++) {
+			output[i * cols + j] = imageMatrix[i][j];
+		}
+	}
+}
+
 void InverseImage(BYTE* Img, BYTE *Out, int W, int H)
 {
 	int ImgSize = W * H;
@@ -690,13 +810,83 @@ void RGB2YCbCr(BYTE* Image, BYTE* Y, BYTE* Cb, BYTE* Cr, int W, int H)
 		}
 	}
 }
+
+void Erosion(BYTE * Image, BYTE * Output, int W, int H)
+{
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) {
+			if (Image[i * W + j] == 255) //전경화소라면
+			{
+				if (!(Image[(i - 1) * W + j] == 255 &&
+					Image[(i + 1) * W + j] == 255 &&
+					Image[i * W + j - 1] == 255 &&
+					Image[i * W + j + 1] == 255)) // 4주변화소가 모두 전경화소가 아니라면
+					Output[i * W + j] = 0;
+				else Output[i * W + j] = 255;
+			}
+			else Output[i * W + j] = 0;
+		}
+	}
+}
+
+void Dilation(BYTE* Image, BYTE* Output, int W, int H)
+{
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) {
+			if (Image[i * W + j] == 0) //배경화소라면
+			{
+				if (!(Image[(i - 1) * W + j] == 0 &&
+					Image[(i + 1) * W + j] == 0 &&
+					Image[i * W + j - 1] == 0 &&
+					Image[i * W + j + 1] == 0)) // 4주변화소가 모두 배경화소가 아니라면
+					Output[i * W + j] = 255;
+				else Output[i * W + j] = 0;
+			}
+			else Output[i * W + j] = 255;
+		}
+	}
+}
+void FeatureExtractThinImage(BYTE* Image, BYTE* Output, int W, int H)
+{
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) {
+			int cnt = 0;
+			if (Image[i * W  + j] == 0) //전경화소라면
+			{
+				if (Image[(i + 1) * W + (j - 1)] == 0 )
+					cnt++;
+				if (Image[(i + 1) * W + j] == 0 )
+					cnt++;
+				if (Image[(i + 1) * W  + (j + 1) ] == 0)
+					cnt++;
+				if (Image[i * W   + (j - 1)] == 0 )
+					cnt++;
+				if (Image[i * W  + (j + 1) ] == 0 )
+					cnt++;
+				if (Image[(i - 1) * W  + (j - 1) ] == 0 )
+					cnt++;
+				if (Image[(i - 1) * W  + (j + 1) ] == 0)
+					cnt++;
+				if (Image[(i - 1) * W + (j)] == 0)
+					cnt++;
+				
+				if (cnt ==3)
+				{
+					Output[i * W  + j] = 255;
+					
+				}
+			}
+			
+		}
+	}
+}
 int main()
 {
 	BITMAPFILEHEADER hf; // 14바이트
 	BITMAPINFOHEADER hInfo; // 40바이트
 	RGBQUAD hRGB[256]; // 1024바이트
 	FILE* fp;
-	fp = fopen("face.bmp", "rb");
+	fp = fopen("dilation.bmp", "rb");
 	if (fp == NULL) {
 		printf("File not found!\n");
 		return -1;
@@ -723,6 +913,25 @@ int main()
 	
 	int Histo[256] = { 0 };
 	int AHisto[256] = { 0 };
+
+	for (int i = 0; i < 256; i++)
+		hRGB[i].rgbRed =
+		hRGB[i].rgbBlue =
+		hRGB[i].rgbGreen =
+		hRGB[i].rgbReserved = i;
+
+	Dilation(Image, Output, W, H);
+	Dilation(Output, Image, W, H);
+	Dilation(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+	Erosion(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+	InverseImage(Image, Image, W, H);
+	zhangSuen(Image, Output, H, W);
+	
+	
+
+	
 
 	/*for (int i = 0; i < W; i++){
 		FillColor(Image, i, 200, W, H, 0, 255, 255);
@@ -809,12 +1018,13 @@ int main()
 	//	}
 	//}
 	
-	BYTE* Y = (BYTE*)malloc(ImgSize);
+	/*BYTE* Y = (BYTE*)malloc(ImgSize);
 	BYTE* Cb = (BYTE*)malloc(ImgSize);
 	BYTE* Cr = (BYTE*)malloc(ImgSize);
 
-	RGB2YCbCr(Image, Y, Cb, Cr, W, H);
+	RGB2YCbCr(Image, Y, Cb, Cr, W, H);*/
 
+	FeatureExtractThinImage(Output, Output, H, W);
 	// 딸기영영만 masking (Y, Cb, Cr 모델 기준)
 	/*for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
@@ -829,70 +1039,70 @@ int main()
 	}*/
 
 	// 얼굴 영역 검출
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			if (Cb[i * W + j] < 125 && Cb[i * W + j] > 95 && Cr[i * W + j] > 155 && Cr[i * W + j] < 185) {
-				Output[i * W * 3 + j * 3] = Image[i * W * 3 + j * 3];
-				Output[i * W * 3 + j * 3 + 1] = Image[i * W * 3 + j * 3 + 1];
-				Output[i * W * 3 + j * 3 + 2] = Image[i * W * 3 + j * 3 + 2];
-			}
-			else
-				Output[i * W * 3 + j * 3] = Output[i * W * 3 + j * 3 + 1] = Output[i * W * 3 + j * 3 + 2] = 0;
-		}
-	}
+	//for (int i = 0; i < H; i++) {
+	//	for (int j = 0; j < W; j++) {
+	//		if (Cb[i * W + j] < 125 && Cb[i * W + j] > 95 && Cr[i * W + j] > 155 && Cr[i * W + j] < 185) {
+	//			Output[i * W * 3 + j * 3] = Image[i * W * 3 + j * 3];
+	//			Output[i * W * 3 + j * 3 + 1] = Image[i * W * 3 + j * 3 + 1];
+	//			Output[i * W * 3 + j * 3 + 2] = Image[i * W * 3 + j * 3 + 2];
+	//		}
+	//		else
+	//			Output[i * W * 3 + j * 3] = Output[i * W * 3 + j * 3 + 1] = Output[i * W * 3 + j * 3 + 2] = 0;
+	//	}
+	//}
 
-	int lx=W *3, ly = H*3, rx = 0, ry = 0;
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			if (Output[i * W * 3 + j * 3] != 0
-				&& Output[i * W * 3 + j * 3 + 1] != 0
-				&& Output[i * W * 3 + j * 3 + 2] != 0) {
-				if (lx >= j)
-					lx = j;
-				
-				if(ly >= i)
-					ly = i;
-				
-				if (rx <= j)
-					rx = j;
+	//int lx=W *3, ly = H*3, rx = 0, ry = 0;
+	//for (int i = 0; i < H; i++) {
+	//	for (int j = 0; j < W; j++) {
+	//		if (Output[i * W * 3 + j * 3] != 0
+	//			&& Output[i * W * 3 + j * 3 + 1] != 0
+	//			&& Output[i * W * 3 + j * 3 + 2] != 0) {
+	//			if (lx >= j)
+	//				lx = j;
+	//			
+	//			if(ly >= i)
+	//				ly = i;
+	//			
+	//			if (rx <= j)
+	//				rx = j;
 
-				if (ry <= i)
-					ry = i;
-			}
-		}
-	}
+	//			if (ry <= i)
+	//				ry = i;
+	//		}
+	//	}
+	//}
 
-	// 얼굴 원상복귀 
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			Output[i * W * 3 + j * 3] = Image[i * W * 3 + j * 3];
-			Output[i * W * 3 + j * 3 + 1] = Image[i * W * 3 + j * 3 + 1];
-			Output[i * W * 3 + j * 3 + 2] = Image[i * W * 3 + j * 3 + 2];
-		}
-	}
+	//// 얼굴 원상복귀 
+	//for (int i = 0; i < H; i++) {
+	//	for (int j = 0; j < W; j++) {
+	//		Output[i * W * 3 + j * 3] = Image[i * W * 3 + j * 3];
+	//		Output[i * W * 3 + j * 3 + 1] = Image[i * W * 3 + j * 3 + 1];
+	//		Output[i * W * 3 + j * 3 + 2] = Image[i * W * 3 + j * 3 + 2];
+	//	}
+	//}
+	//
+	//// x,y 좌표 기준으로 선 그어 사각형 만들기
+	//for (int i = 0; i < H; i++) {
+	//	for (int j = 0; j < W; j++) {
+	//		if (j >= lx && j <= rx && ((i == ly) || (i == ry)) ){
+	//			
+	//			Output[i * W * 3 + j * 3 + 2] = 255;
+	//			Output[i * W * 3 + j * 3 + 1] = 0;
+	//			Output[i * W * 3 + j * 3 + 0] = 0;
+	//		}
+	//		if (i >= ly && i <= ry && ((j == lx) || (j == rx))) {
+	//			
+	//			Output[i * W * 3 + j * 3 + 2] = 255;
+	//			Output[i * W * 3 + j * 3 + 1] = 0;
+	//			Output[i * W * 3 + j * 3 + 0] = 0;
+	//		}
+	//	}
+	//}
 	
-	// x,y 좌표 기준으로 선 그어 사각형 만들기
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			if (j >= lx && j <= rx && ((i == ly) || (i == ry)) ){
-				
-				Output[i * W * 3 + j * 3 + 2] = 255;
-				Output[i * W * 3 + j * 3 + 1] = 0;
-				Output[i * W * 3 + j * 3 + 0] = 0;
-			}
-			if (i >= ly && i <= ry && ((j == lx) || (j == rx))) {
-				
-				Output[i * W * 3 + j * 3 + 2] = 255;
-				Output[i * W * 3 + j * 3 + 1] = 0;
-				Output[i * W * 3 + j * 3 + 0] = 0;
-			}
-		}
-	}
-	
 
-	free(Y);
+	/*free(Y);
 	free(Cr);
-	free(Cb);
+	free(Cb);*/
 	/*fp = fopen("Y.raw", "wb");
 	fwrite(Y, sizeof(BYTE), W* H, fp);
 	fclose(fp);
